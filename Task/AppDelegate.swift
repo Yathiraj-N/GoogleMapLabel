42f15ba473d8603068e076854156a04854646001
Yathiraj N <32062991+YathirajN@users.noreply.github.com>
2017-12-22 20:26:42 +0530

    Completed with the task
    +++++++++++++++++
    +Route polyline is drawn from current location to Mumbai Airport
    +Route polyline is drawn from current location to Chennai Airport.
    +Current location label is displayed.

--- Task/AppDelegate.swift
+++ Task/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import GoogleMaps
-import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,12 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
      
         GMSServices.provideAPIKey("AIzaSyDMmYoDFM4sQghwIYGZzAuOTSkv3RnP6QA")
-
-        GMSPlacesClient.provideAPIKey("AIzaSyAVHP8pq_EaLDSQ5_0uy_AIBMnMWd_buzU")
-        
-        
-
-        
         return true
     }
 
--- Task/ViewController.swift
+++ Task/ViewController.swift
@@ -9,30 +9,52 @@
 import UIKit
 
 import GoogleMaps
-import MapKit
 import CoreLocation
 
 class locationViewController: UIViewController, CLLocationManagerDelegate
 {
     let manager = CLLocationManager()
     
-    // Creates a marker in the center of the map.
+    // Creates markers in the map.
     let markerMumbaiAirport = GMSMarker()
     let markerCurrentLocation = GMSMarker()
     let markerChennaiAirport = GMSMarker()
     
+    //Draw route polyline.
     let directionPolyline = GMSPolyline()
-    var bIsShowRoute = true
-
-    var currentLocCoordinates = CLLocationCoordinate2D(latitude: 12.9503928, longitude: 77.5755419)
+    
+    //Flag to switch between routes.
+    var bIsSwitchRoute = false
+   
+    //Location coordinates.
+    var currentLocCoordinates = CLLocationCoordinate2D()
+    let mumbaiLoc = CLLocationCoordinate2D(latitude: 19.089560, longitude: 72.865614)
+    let chennaiLoc = CLLocationCoordinate2D(latitude: 12.994112, longitude: 80.170867)
+    
+    //Button 'Switch Route'.
+    let btnSwitchRoute = UIButton()
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        //Set button properties.
+        btnSwitchRoute.frame = CGRect(x: 20,
+                                      y: 50,
+                                      width: 140,
+                                      height: 44)   
+        btnSwitchRoute.setTitle("Switch Route", for: .normal)
+        btnSwitchRoute.backgroundColor = UIColor.blue
+        btnSwitchRoute.setTitleColor(UIColor.white, for: .normal)
+        btnSwitchRoute.setTitleColor(UIColor.white.withAlphaComponent(0.3),
+                                         for: .highlighted)
+        btnSwitchRoute.addTarget(self,
+                                 action: #selector(btnSwitchAction),
+                                 for: .touchUpInside)
+
         manager.delegate = self
         manager.requestAlwaysAuthorization()
         
+        //Check Access and load map view.
         if CLLocationManager.locationServicesEnabled() {
             manager.desiredAccuracy = kCLLocationAccuracyBest
             manager.startUpdatingLocation()
@@ -47,53 +69,54 @@ class locationViewController: UIViewController, CLLocationManagerDelegate
         }
     }
     
-    override func loadView() {
-        
-        let mumbaiLoc = CLLocationCoordinate2D(latitude: 19.089560, longitude: 72.865614)
-        let chennaiLoc = CLLocationCoordinate2D(latitude: 12.994112, longitude: 80.170867)
-        
-        if bIsShowRoute
-        {
-            getPolylineRoute( from: mumbaiLoc, to: chennaiLoc)
-        }
-
-        // Create a GMSCameraPosition.
-        let camera = GMSCameraPosition.camera(withLatitude: 12.917382, longitude: 77.578154, zoom: 9.0)
+    override func loadView()
+    {
+        // Create GMSCameraPosition.
+        let camera = GMSCameraPosition.camera(withLatitude: 12.917382,
+                                              longitude: 77.578154, zoom: 9.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         
         mapView.isMyLocationEnabled = true
         
-        //Current location with Green color marker.
-        markerCurrentLocation.title = "Current"
-        markerCurrentLocation.snippet = "Location"
+        //Current location with 'Green' color marker.
+        markerCurrentLocation.title = "Current Location"
         markerCurrentLocation.map = mapView
         markerCurrentLocation.icon = GMSMarker.markerImage(with: UIColor.green)
         
-        //Mumbai Airport marker. - RED color
-        markerMumbaiAirport.position = CLLocationCoordinate2D(latitude: 19.089560, longitude: 72.865614)
-        markerMumbaiAirport.title = "Mumbai"
-        markerMumbaiAirport.snippet = "India"
+        //Mumbai Airport location with 'Red' color marker.
+        markerMumbaiAirport.position = CLLocationCoordinate2D(latitude: 19.089560,
+                                                              longitude: 72.865614)
+        markerMumbaiAirport.title = "Shivaji International Airport"
+        markerMumbaiAirport.snippet = " Ville Parle East, Mumbai, India, 400070"
         markerMumbaiAirport.map = mapView
         markerMumbaiAirport.icon = GMSMarker.markerImage(with: UIColor.red)
 
-        //Chennai Airport markwer. - BLUE color
-        markerChennaiAirport.position = CLLocationCoordinate2D(latitude: 12.994112, longitude: 80.170867)
-        markerChennaiAirport.title = "Chennai"
-        markerChennaiAirport.snippet = "India"
+        //Chennai Airport location with 'Blue' color marker.
+        markerChennaiAirport.position = CLLocationCoordinate2D(latitude: 12.994112,
+                                                               longitude: 80.170867)
+        markerChennaiAirport.title = "Chennai International Airport"
+        markerChennaiAirport.snippet = "Minambakkam, Sholinganallur, India, 601213"
         markerChennaiAirport.map = mapView
         markerChennaiAirport.icon = GMSMarker.markerImage(with: UIColor.blue)
         
         self.view = mapView
         
-        if bIsShowRoute
-        {
-            directionPolyline.map = mapView
-        }
+        //Add button 'Switch Route as sub view to map view.
+        mapView.addSubview(btnSwitchRoute)
+        
+        //Update direction polyline.
+        directionPolyline.map = mapView
+    }
+    
+    //Function to switch between routes.
+    func btnSwitchAction()
+    {
+        bIsSwitchRoute = !(bIsSwitchRoute)
     }
     
     //Display polyline route.
-    func getPolylineRoute(from source: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D){
-        
+    func getPolylineRoutes(from source: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D)
+    {
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
         
@@ -114,17 +137,23 @@ class locationViewController: UIViewController, CLLocationManagerDelegate
                         let dic = array[0] as! NSDictionary
                         let dic1 = dic["overview_polyline"] as! NSDictionary
                         let points = dic1["points"] as! String
-                        
-                        print(points)
-                        
+                            
                             DispatchQueue.main.async {
                                 
                                 let pathway = GMSPath(fromEncodedPath: points)
                                 
                                 self.directionPolyline.path = pathway
                                 
-                                self.directionPolyline.strokeWidth = 2.0
-                                self.directionPolyline.strokeColor = UIColor.cyan
+                                self.directionPolyline.strokeWidth = 3.0
+                                
+                                if (self.bIsSwitchRoute)
+                                {
+                                    self.directionPolyline.strokeColor = UIColor.red
+                                }
+                                else
+                                {
+                                    self.directionPolyline.strokeColor = UIColor.blue
+                                }
                             }
                         }
                     }
@@ -136,11 +165,10 @@ class locationViewController: UIViewController, CLLocationManagerDelegate
         task.resume()
     }
   
-    //Display Address.
-    func getAddressFromLatLon(pdblLatitude: String, withLongitude pdblLongitude: String) -> String
+    //Display Coordinate Address.
+    func getAddressFromLatLons(pdblLatitude: String, withLongitude pdblLongitude: String)
     {
         var addressString : String = ""
-        
         var center : CLLocationCoordinate2D = CLLocationCoordinate2D()
         let lat: Double = Double("\(pdblLatitude)")!
         //21.228124
@@ -151,7 +179,7 @@ class locationViewController: UIViewController, CLLocationManagerDelegate
         center.longitude = lon
         
         let loc: CLLocation = CLLocation(latitude:center.latitude, longitude: center.longitude)
-        
+
         ceo.reverseGeocodeLocation(loc, completionHandler:
             {(placemarks, error) in
                 if (error != nil)
@@ -177,24 +205,41 @@ class locationViewController: UIViewController, CLLocationManagerDelegate
                     if pm.postalCode != nil {
                         addressString = addressString + pm.postalCode! + " "
                     }
-                    
-                    print(addressString)
+                    self.markerCurrentLocation.snippet = addressString
                 }
         })
-        return addressString
     }
-    
+  
+    //Update current location.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-       
+       //Update current location marker.
         markerCurrentLocation.position = locations[0].coordinate
-
+       
+        //Update current location coordinates.
         currentLocCoordinates = locations[0].coordinate
+       
+        //Update current location address.
+        getAddressFromLatLons(pdblLatitude: "\(locations[0].coordinate.latitude)",
+            withLongitude: "\(locations[0].coordinate.longitude)" )
+       
+        //To switch routes.
+        if bIsSwitchRoute
+        {
+            getPolylineRoutes( from: currentLocCoordinates, to: mumbaiLoc)
+        }
+        else
+        {
+            getPolylineRoutes( from: currentLocCoordinates, to: chennaiLoc)
+        }
     }
     
+    //AuthorizationStatus.
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         if status == .authorizedAlways {
-            //Load
+            //Load map view.
             loadView()
         }
     }
 }
+
+
